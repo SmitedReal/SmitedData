a76486c06393b21013e70e9bbd0e27e4efaf9ac6
SmitedReal <uvojtiska@gmail.com>
2025-07-22 11:39:01 +0200

    SmitedData 1.1
    SDAT class should now work
    example changed
    code simplified, made better
    
    PLEASE DONT USE THIS LIBARY IT SUCKS ITS JUST A JOKE

--- src/main/java/example.java
+++ src/main/java/example.java
@@ -1,25 +1,11 @@
-import java.io.IOException;
-
 public class example {
-    public static void main(String[] args) {
-        // Create sample data
-        SDAT data = SDAT.object()
-                .put("name", "John Doe")
-                .put("secretData", "This is confidential!")
-                .put("numbers", SDAT.array().add(1).add(2).add(3));
-
-        try {
-            // Save to binary file
-            data.saveToFile("data.sdat");
-            System.out.println("Saved data to data.sdat");
-
-            // Load from binary file
-            SDAT loaded = SDAT.loadFromFile("data.sdat");
-            System.out.println("Loaded data:");
-            System.out.println(loaded.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public static void main(String[] args) {
+   // declaring a new SDAT object creates a file,
+   // if it does not already exist.
+   SDAT data = new SDAT("data.txt");
+   // This thing prints out the lines in the file.
+   for (int i = 0; i < data.lineCount(); i++) {
+       System.out.println((i + 1) + ": " + data.readLine(i));
+   }
+}
 }
